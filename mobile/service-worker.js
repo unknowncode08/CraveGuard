1ddf043564a66b67d71e13109874d1cfa5fcd6c4
oesoliman <oesoliman@gmail.com>
2025-05-06 16:27:33 +0000

    update

---
+++ mobile/service-worker.js
@@ -0,0 +1,1 @@
+const CACHE = 'craveguard'; const ASSETS = ['/', '/index.html', '/styles.css', '/script.js']; self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); }); self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
